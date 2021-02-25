fef82f534937fb3457c9665a63e23f3073a12e1e
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-02-26 00:08:25 +0530

    Create Queue.java

---
+++ src/main/java/org/DataStructure/Queue.java
@@ -0,0 +1,13 @@
+public class Queue<T> {
+	MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+
+	/* push the Node to stack */
+	public void enqueue(INode newNode) {
+		myLinkedList.addLast(newNode);
+	}
+
+	/* peek the top node in stack */
+	public INode peek() {
+		return myLinkedList.head;
+	}
+}
